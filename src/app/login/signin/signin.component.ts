a3c2b3bcccb6392c693b23c1993a4369feb1e42b
Jacob Webb <jacob@jacob-webb.com>
2020-12-02 07:02:15 -0800

    Removed getUser from signing component
    Attempting to troubleshoot retrieval of user information. Receiving an error when calling backend to retrieve user

--- src/app/login/signin/signin.component.ts
+++ src/app/login/signin/signin.component.ts
@@ -5,7 +5,7 @@ import { SIGNIN_TOKEN } from 'src/app/app.constants';
 import { User } from 'src/app/list-users/list-users.component';
 import { Role } from 'src/app/role/role.model';
 import { RoleService } from 'src/app/role/role.service';
-import { StorageService } from 'src/app/service/data/storage.service';
+import { FIRST_NAME, LAST_NAME, StorageService, USER_ROLE } from 'src/app/service/data/storage.service';
 import { UserDataService } from 'src/app/service/data/user-data.service';
 import { PreloginService } from 'src/app/service/prelogin.service';
 import { BasicAuthenticationService } from '../../service/basic-authentication.service';
@@ -69,17 +69,19 @@ export class SigninComponent implements OnInit {
     * Once User is registered, retrieve the first and last name
     * as well as the Role that the user is given for this app
     */
-    this.userService.retrieveUserByEmail(this.email).subscribe(
-      response => {
-        // localStorage.setItem('firstName', response.firstName);
-        // localStorage.setItem('lastName', response.lastName);
-        // localStorage.setItem('role', this.getUserRole(response.roles, this.roleService.appRoles))
-      }
-    )}
+    // this.userService.retrieveUserByEmail(this.email).subscribe(
+    //   response => {
+    //     this.storageService.setStorageItem(FIRST_NAME, response.firstName);
+    //     this.storageService.setStorageItem(LAST_NAME, response.lastName);
+    //     this.storageService.setStorageItem(USER_ROLE, this.getUserRole(response.roles, this.roleService.appRoles));
+    //   }
+    // )
+  }
 
   /* If possible, return the user's role that matches one of the apps
   * possible roles, otherwise return null
   */
+ /*
   getUserRole(roles: Role[], possibleRoles: string[]): string {
 
     //for each role of the user, check if there is a match with the possibe
@@ -92,5 +94,6 @@ export class SigninComponent implements OnInit {
 
     return userRole;
   }
+  */
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -8,4 +8,22 @@ export class UserService {
   user: User;
 
   constructor() { }
+
+    /* If possible, return the user's role that matches one of the apps
+  * possible roles, otherwise return null
+  */
+ /*
+  getUserRole(roles: Role[], possibleRoles: string[]): string {
+
+    //for each role of the user, check if there is a match with the possibe
+    //roles from the app.
+    var userRole: string = '';
+    possibleRoles.forEach(auth => {
+       var found = roles.find(role => role.name.valueOf() == auth.valueOf())
+       if (found != null) userRole = found.name;
+    })
+
+    return userRole;
+  }
+  */
 }
