0429228f31f29279dc39424fdbbd9dc17af521f3
Jacob Webb <jacob@jacob-webb.com>
2020-12-09 05:48:33 -0800

    Made todoList as an Observable from todoData Subject

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ import { TodoDataService } from '../service/data/todo-data.service';
 export class HomeComponent implements OnInit {
 
   userEmail: string;
-  todos$: Observable<Todo[]>
+  todoList$: Observable<Todo[]>
   todosNotDone$: Observable<Todo[]>;
   todosDone$: Observable<Todo[]>;
 
@@ -27,9 +27,9 @@ export class HomeComponent implements OnInit {
 
     this.storageService.watchStorageItem(AUTHENTICATED_USER).subscribe(data => this.userEmail = data);
 
-    this.todos$ = this.todoDataService.retrieveAllTodos(this.userEmail);
-    this.todos$.subscribe(data => {
-      this.todoList = data
+    this.todoList$ = this.todoDataService.retrieveAllTodos(this.userEmail);
+    this.todoList$.subscribe(list => {
+      this.todoList = list
     });
 
   }
